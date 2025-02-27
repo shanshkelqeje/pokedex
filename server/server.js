2881cc7a595ff0bdc55ddfab3ec2efa05fdddd97
shanshkelqeje <118238788+shanshkelqeje@users.noreply.github.com>
2025-02-27 13:31:22 +0800

    implement MongoDB integration and setup Pokemon schema

--- server/server.js
+++ server/server.js
@@ -1,10 +1,76 @@
+const mongoose = require("mongoose");
 const express = require("express");
 
 const app = express();
 const port = 3000;
 
+main = async () => {
+    await mongoose.connect(
+        "mongodb+srv://shanshkelqeje:41TfhtdVAMrHQL31@pokedb.ygio4.mongodb.net/?retryWrites=true&w=majority&appName=PokeDB",
+        { dbName: "pokedex" }
+    );
+};
+
+main().catch((err) => console.log(err));
+
+const pokemonSchema = new mongoose.Schema(
+    {
+        _id: Number,
+        name: String,
+        height: Number,
+        weight: Number,
+        abilities: [String],
+        sprite: String,
+        cry: String,
+        stats: {
+            hp: Number,
+            attack: Number,
+            defense: Number,
+            special_attack: Number,
+            special_defense: Number,
+            speed: Number,
+        },
+        types: [String],
+    },
+    { collection: "pokemon" }
+);
+
+const Pokemon = mongoose.model("Pokemon", pokemonSchema);
+
 app.get("/", async (req, res) => {
-    var pokemon = await getPokemon();
+    const json = await getPokemon();
+    const {
+        id,
+        name,
+        height,
+        weight,
+        abilities,
+        sprites,
+        cries,
+        stats,
+        types,
+    } = json;
+
+    const pokemon = new Pokemon({
+        _id: id,
+        name,
+        height,
+        weight,
+        abilities: abilities.map((slot) => slot.ability.name),
+        sprite: sprites.front_default,
+        cry: cries.legacy,
+        stats: {
+            hp: stats[0].base_stat,
+            attack: stats[1].base_stat,
+            defense: stats[2].base_stat,
+            special_attack: stats[3].base_stat,
+            special_defense: stats[4].base_stat,
+            speed: stats[5].base_stat,
+        },
+        types: types.map((slot) => slot.type.name),
+    });
+
+    await pokemon.save();
     res.send(pokemon);
 });
 
